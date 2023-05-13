ee9e8f7be54712b14387fca12e8048fe3f822080
Jain <priyank.jain@sms-group.com>
2023-05-13 19:17:43 +0530

    created requirements and setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(filepath:str)-> List[str]:
+      with open(filepath, 'r') as fileobj:
+            requirements = fileobj.readlines()
+      requirements = [requirement.strip('\n') for requirement in requirements]
+
+      if('-e .' in requirements):
+            requirements.remove('-e .')
+
+      return requirements
+
+
+setup(name='End To End Machine Learning Project',
+      version='0.0.1',
+      description='Python Distribution Utilities',
+      author='Priyank Jain',
+      author_email='priyank.jain@sms-group.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages = find_packages(),
+      install_requires = get_requirements('requirements.txt')
+     )
